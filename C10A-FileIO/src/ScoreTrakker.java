727380e999f759bbb6238df20e57d8414430cf79
Vincent Nguyen <vnguyen2@mines.edu>
2025-10-02 15:23:43 -0600

    Added file modifications.

--- C10A-FileIO/src/ScoreTrakker.java
+++ C10A-FileIO/src/ScoreTrakker.java
@@ -1,45 +1,55 @@
 import java.util.ArrayList;
-import java.io.File;
+import java.io.FileReader;
 import java.util.Collections;
 import java.util.Scanner;
 
+import java.io.FileNotFoundException;
 
 public class ScoreTrakker {
-	private ArrayList<Student> student = new ArrayList<Student>();
+	private ArrayList<Student> students = new ArrayList<Student>();
 
 	
-	public ScoreTrakker(ArrayList<Student> student) {
+	public ScoreTrakker(ArrayList<Student> students) {
 		super();
-		this.student = student;
+		this.students = students;
 	}
 	
-	public void loadDataFile(String filename) {
-		try (Scanner scanner = new Scanner() {
-			while(scanner.hasNextLine()) {
-				String dataString = scanner.nextLine();
-				String line = scanner.nextLine();
-				String[] parts = line.split(",");
-				
-				String name = parts[0];
-				//String surname = parts[1];
-				int score = Integer.parseInt(parts[2]);
-				
+	public void loadDataFile(String fileName) {
+		FileReader reader = null;
+		Scanner in = null;
+		try {
+			reader = new FileReader(fileName);
+			in = new Scanner(reader);
+			while(in.hasNextLine()) {
+				String name = in.nextLine();
+				String strScore = in.nextLine();
+				int score = Integer.parseInt(strScore);
 				Student s = new Student(name, score);
+				students.add(s);
 				
 			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
 		}
 	}
 
 	public void printInOrder(ArrayList<Student> student) {
-		Collections.sort(student);
+		Collections.sort(students);
+		for (Student aStudent : students) {
+			System.out.println(aStudent.toString());
+		}
 	}
 	
-	public void processFiles() {
+	public void processFiles(String fileName) {
+		loadDataFile(fileName);
+		printInOrder(students);
 		
 	}
 	
-	public void main() {
-		
+	public void main(String[] args) {
+		ScoreTrakker tracker = new ScoreTrakker(students);
+		String fileName = "scores.txt";
+		tracker.processFiles(fileName);
 	}
 }
 
